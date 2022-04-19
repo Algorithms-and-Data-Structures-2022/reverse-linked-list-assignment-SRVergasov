343f19bf77b71008874e73bd7caa75dc69158f03
SRVergasov <SRVergasov@stud.kpfu.ru>
2022-04-19 17:42:41 +0300

    completed

--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -1,6 +1,6 @@
 #include "assignment/linked_list.hpp"
 
-#include <utility> // swap
+#include <utility>  // swap
 
 namespace assignment {
 
@@ -12,7 +12,18 @@ namespace assignment {
    *    по времени ~ O(N)
    */
   void LinkedList::ReverseIterative() {
-    // Напишите здесь свой код ...
+    Node* previous = nullptr;
+    Node* curr = front_;
+    back_ = front_;
+
+    while (curr != nullptr) {
+      Node* next = curr->next;
+      (*curr).next = previous;
+
+      previous = curr;
+      curr = next;
+    }
+    front_ = previous;
   }
 
   /**
@@ -23,12 +34,19 @@ namespace assignment {
    *    по времени ~ O(N)
    */
   void LinkedList::ReverseRecursive() {
-    // Напишите здесь свой код ...
+    reverse_recursive_helper(front_, nullptr);
   }
 
   // вспомогательный метод для реализации рекурсии
   void LinkedList::reverse_recursive_helper(Node*& curr, Node* prev) {
-    // Напишите здесь свой код ...
+    if (curr == nullptr) {
+      back_ = front_;
+      front_ = prev;
+      return;
+    }
+    Node* cur = curr->next;
+    curr->next = prev;
+    reverse_recursive_helper(cur, curr);
   }
 
 }  // namespace assignment
